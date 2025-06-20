5e2e2eb25e3c7d3223bf9c639d42fc0a5b02def8
DieselRo <160371424+DieselRo@users.noreply.github.com>
2025-06-20 13:03:09 -0500

    feat: add central app builder

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
-#![allow(dead_code)]
-
 use bevy::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod states;
+mod core;
+mod player;
+mod enemy;
+mod ui;
+mod editor;
 
 use core::plugin::CorePlugin;
 use player::plugin::PlayerPlugin;
-
-mod core;
-mod editor;
-mod enemy;
-mod player;
-mod states;
-mod ui;
+use enemy::plugin::EnemyPlugin;
+use ui::plugin::UiPlugin;
+use editor::plugin::EditorPlugin;
 
 fn main() {
-    let mut app = App::new();
-    app.add_plugins(DefaultPlugins)
+    App::new()
+        .add_plugins(DefaultPlugins)
         .init_state::<states::AppState>()
         .add_plugins(CorePlugin)
         .add_plugins(PlayerPlugin)
-        .add_plugins(enemy::plugin::EnemyPlugin)
-        .add_plugins(ui::plugin::UiPlugin); // .add_plugins(editor::plugin::EditorPlugin)
-
-    if cfg!(debug_assertions) {
-        app.add_plugins(WorldInspectorPlugin::new());
-    }
-
-    app.run();
+        .add_plugins(EnemyPlugin)
+        .add_plugins(UiPlugin)
+        // .add_plugins(EditorPlugin)
+        .run();
 }
